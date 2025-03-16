a65a2c623c8295f88d3dbf07d6d6c679d4ee25cc
manupanand <manupanandindianinvestor@gmail.com>
2025-03-16 18:29:42 +0530

    githubactions-repo-commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM    docker.io/redhat/ubi9-minimal:latest
+
+RUN     microdnf module disable nginx  -y && microdnf module enable nginx:1.24 -y && microdnf install nginx gettext-y 
+RUN     rm -rf /usr/share/nginx/html/*
+COPY    /static/  /usr/share/nginx/html/
+COPY    nginx.conf /opt/nginx.conf 
+COPY    run.sh  / 
+ENTRYPOINT [ "bash","run.sh" ]
+
+
---
+++ run.sh
@@ -0,0 +1,60 @@
+# to make sure that environment variables are present before running image
+
+
+if [ -z "${CATALOGUE_HOST}"]; then
+    echo Input CATALOGUE_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${CATALOGUE_PORT}"]; then
+    echo Input CATALOGUE_PORT is missing
+    exit 1
+fi
+if [ -z "${USER_HOST}"]; then
+    echo Input USER_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${USER_PORT}"]; then
+    echo Input USER_PORT is missing
+    exit 1
+fi
+if [ -z "${CART_HOST}"]; then
+    echo Input CART_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${CART_PORT}"]; then
+    echo Input CART_PORT is missing
+    exit 1
+fi
+if [ -z "${SHIPPING_HOST}"]; then
+    echo Input SHIPPING_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${SHIPPING_PORT}"]; then
+    echo Input SHIPPING_PORT is missing
+    exit 1
+fi
+if [ -z "${PAYMENT_HOST}"]; then
+    echo Input PAYMENT_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${PAYMENT_PORT}"]; then
+    echo Input PAYMENT_PORT is missing
+    exit 1
+fi
+
+
+# repalce env variables with URL in nginx.conf
+#envsubst -gettext package
+cat /opt/nginx.conf | envsubst > /etc/nginx/nginx.conf
+
+nginx -g 'daemon off;'
