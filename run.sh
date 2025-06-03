afc394d5b41637a84bceff2c291383b38d06f5d0
manupanand <manupanandindianinvestor@gmail.com>
2025-06-03 14:23:25 +0530

    frontend

--- run.sh
+++ run.sh
@@ -2,53 +2,53 @@
 # to make sure that environment variables are present before running image
 
 
-if [ -z "${CATALOGUE_HOST}"]; then
+if [ -z "${CATALOGUE_HOST}" ]; then
     echo Input CATALOGUE_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${CATALOGUE_PORT}"]; then
+if [ -z "${CATALOGUE_PORT}" ]; then
     echo Input CATALOGUE_PORT is missing
     exit 1
 fi
-if [ -z "${USER_HOST}"]; then
+if [ -z "${USER_HOST}" ]; then
     echo Input USER_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${USER_PORT}"]; then
+if [ -z "${USER_PORT}" ]; then
     echo Input USER_PORT is missing
     exit 1
 fi
-if [ -z "${CART_HOST}"]; then
+if [ -z "${CART_HOST}" ]; then
     echo Input CART_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${CART_PORT}"]; then
+if [ -z "${CART_PORT}" ]; then
     echo Input CART_PORT is missing
     exit 1
 fi
-if [ -z "${SHIPPING_HOST}"]; then
+if [ -z "${SHIPPING_HOST}" ]; then
     echo Input SHIPPING_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${SHIPPING_PORT}"]; then
+if [ -z "${SHIPPING_PORT}" ]; then
     echo Input SHIPPING_PORT is missing
     exit 1
 fi
-if [ -z "${PAYMENT_HOST}"]; then
+if [ -z "${PAYMENT_HOST}" ]; then
     echo Input PAYMENT_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${PAYMENT_PORT}"]; then
+if [ -z "${PAYMENT_PORT}" ]; then
     echo Input PAYMENT_PORT is missing
     exit 1
 fi
